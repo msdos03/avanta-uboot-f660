774fec71d5b0e09e4277ca3fd607f257af588f24
msdos03 <3365853196@qq.com>
2021-08-28 00:20:44 -0700

    remove some sgd only envs

--- sg200_extra.h
+++ sg200_extra.h
@@ -1,31 +1,15 @@
-
-#define SGD_MODEL_VER "sgd_revision=3A\0"
-#define SGD_FW_VER "sgd_fw=1.0.7\0"
-#define SGD_MAGIC "sgd_magic=0xc00010\0"
-
-/*   start SG200 extra */
-
 /*
  * Default environment variables
  */
 #define CONFIG_BOOTCOMMAND             "nand read 2000000 100000 200000;bootm"
 
 /* #define CONFIG_MTDPARTS                "mtdparts=nand:2m(uboot),-(ubi)" */
-#define CONFIG_MTDPARTS "mtdparts=nand:1m(uboot),-(ubi)"
-#define MTDPARTS "1m(uboot),-(ubi)"
+#define CONFIG_MTDPARTS "mtdparts=nand:1m(uboot),-(firmware)"
+#define MTDPARTS "1m(uboot),-(firmware)"
 
 #define CONFIG_EXTRA_ENV_SETTINGS      "mtdids=nand0=nand\0" \
-       "ubi_root=active\0" \
-       SGD_MAGIC \
        "mtdparts=mtdparts=nand:" MTDPARTS "\0" \
-       "nandparts=mtdparts=nand_mtd:" MTDPARTS "\0" \
-       "sgd_type=AEIServiceGateway\0" \
-       "sgd_model=SG200\0" \
-       "sgd_serial=not set\0" \
-       SGD_MODEL_VER \
-       SGD_FW_VER \
-       "zhomeid=00,00,00,00\0"
-
+       "nandparts=mtdparts=nand_mtd:" MTDPARTS "\0"
 /* UBI file system */
 
 #define CONFIG_CMD_FAT
@@ -39,4 +23,3 @@
 #define CONFIG_CMD_MTDPARTS
 #define CONFIG_LZO
 
-/**** End of SG 200 extra */
