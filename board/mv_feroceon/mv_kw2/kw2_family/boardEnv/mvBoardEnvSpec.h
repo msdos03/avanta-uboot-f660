dd679dfeaae0ca5f48cb09b3a9129b19f19b658f
msdos03 <3365853196@qq.com>
2021-08-31 04:53:29 -0700

    Change MPP config for ZTE F660/F460 v3

--- board/mv_feroceon/mv_kw2/kw2_family/boardEnv/mvBoardEnvSpec.h
+++ board/mv_feroceon/mv_kw2/kw2_family/boardEnv/mvBoardEnvSpec.h
@@ -312,47 +312,46 @@ MPP#	NAME			IN/OUT
 /* HGG420N */
 #define HGG420N_MPP0_7		0x11111111
 #define HGG420N_MPP8_15		0x31111111
-#define HGG420N_MPP16_23		0x41010113
-#define HGG420N_MPP24_31		0x04422220
+#define HGG420N_MPP16_23		0x03003303
+#define HGG420N_MPP24_31		0x00022222
 #define HGG420N_MPP32_39		0x00444444
-#define HGG420N_MPP40_47		0x00006060
-#define HGG420N_MPP48_55		0x00000460
-#define HGG420N_MPP56_63		0x00000000
-#define HGG420N_MPP64_71		0x00500000
+#define HGG420N_MPP40_47		0x50000000
+#define HGG420N_MPP48_55		0x00000000
+#define HGG420N_MPP56_63		0x90909090
+#define HGG420N_MPP64_71		0x00500900
 #define HGG420N_MPP72_79		0x00000000
-#define HGG420N_MPP80_87		0x00000000
+#define HGG420N_MPP80_87		0x00000070
 #define HGG420N_MPP88_88		0x00000000
 
 /*
-HGG420N GPIO map:
+ZTE F460/F660 GPIO map:
 bank0:
- gpio23: net port 3
+ gpio17: warning
+ gpio21: voice1
 bank1:
- gpio9: net port 1
- gpio10: GPON
- gpio11: iTV (net port 2)
- gpio12: PON signal
- gpio13: voice 1
- gpio14: voice 2
- gpio15: network
- gpio17: net port 4
- gpio25: USB
- gpio26: power (active high)
- gpio28: WPS (red)
+ gpio17: pon network
+ gpio18: pon port
+ gpio20: voice2
+ gpio25: ethernet 1
+ gpio27: iTV (ethernet 2)
+ gpio29: ethernet 4
+ gpio31: ethernet 3
 bank2:
- gpio16: WPS (green)
+ gpio11: WPS (red)
+ gpio12: WPS (green)
+ gpio13: USB
 */
 
 /*this section controls which GPIO is set to output mode
 change the bit to 0 to set GPIO to output*/
-#define HGG420N_GPP_OUT_ENA_LOW 0x80080000/*you can find it at f1018104*/
-#define HGG420N_GPP_OUT_ENA_MID 0x69390000/*you can find it at f1018144*/
-#define HGG420N_GPP_OUT_ENA_HIGH 0x00000100/*you can find it at f1018184*/
+#define HGG420N_GPP_OUT_ENA_LOW 0x80100000/*you can find it at f1018104*/
+#define HGG420N_GPP_OUT_ENA_MID 0x54097820/*you can find it at f1018144*/
+#define HGG420N_GPP_OUT_ENA_HIGH 0x000183ab/*you can find it at f1018184*/
 
 /*this section controls the default level of each GPIO*/
-#define HGG420N_GPP_OUT_VAL_LOW  0x01a00000/*you can find it at f1018100*/
-#define HGG420N_GPP_OUT_VAL_MID	 0x1612fe00/*you can find it at f1018140*/
-#define HGG420N_GPP_OUT_VAL_HIGH 0x001b3838/*you can find it at f1018180*/
+#define HGG420N_GPP_OUT_VAL_LOW  0x40220000/*you can find it at f1018100*/
+#define HGG420N_GPP_OUT_VAL_MID	 0x00760000/*you can find it at f1018140*/
+#define HGG420N_GPP_OUT_VAL_HIGH 0x0000f800/*you can find it at f1018180*/
 
 /*this section controls which GPIO is inverted*/
 #define HGG420N_GPP_POL_LOW		0x0
